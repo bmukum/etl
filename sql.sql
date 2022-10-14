29b670c4ce6d0771c7bc5a81a8a6688cc451c9f1
Brandon Mukum <64148017+bmukum@users.noreply.github.com>
2022-10-14 13:12:20 -0400

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -5,13 +5,28 @@ create table detail_table (
 	last_name varchar(50),
 	email varchar(60),
 	payment_id integer,
-	amount integer
+	amount numeric
 );
 
 DROP table if exists summary_table;
 CREATE table summary_table 
 	(
 	customer_name varchar(70),
-	total_amount integer
-)
+	total_amount numeric
+);
 
+insert into detail_table (
+	customer_id,
+	first_name,
+	last_name,
+	email,
+	payment_id,
+	amount
+)
+select
+	c.customer_id, c.first_name, c.last_name, c.email,
+	p.payment_id, p.amount
+FROM payment as P
+INNER JOIN customer AS c ON c.customer_id = p.customer_id;
+
+select * from payment;
